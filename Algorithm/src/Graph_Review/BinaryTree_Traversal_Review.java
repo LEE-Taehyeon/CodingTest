49cd4fe6553ff9062c14f72f6dcbc4f0f730b5b6
LEETaehyeon <merongvvvv@naver.com>
2020-08-06 23:32:48 +0900

    Algorithm - BinaryTree Travarsal_Review

---
+++ Algorithm/src/Graph_Review/BinaryTree_Traversal_Review.java
@@ -0,0 +1,16 @@
+package Graph_Review;
+
+//이진트리 탐색(BinaryTree Traversal) - 전위, 중위, 후위
+//           (1)
+//     (2)          (3)
+// (4)   (5)      ()   (7)
+//()()  ()(10)   ()() (13)()    
+//
+//preOrder(Root, Left, Right) - 1 2 4 5 10 3 7 13 
+//inOrder(Left, Root, Right) - 4 2 5 10 1 3 13 7 
+//postOrder(Left, Right, Root) - 4 10 5 2 13 7 3 1 
+public class BinaryTree_Traversal_Review {
+	public static void main(String[] args) {
+		
+	}
+}
