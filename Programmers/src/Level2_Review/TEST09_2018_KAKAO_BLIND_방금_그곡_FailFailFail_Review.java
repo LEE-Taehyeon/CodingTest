7991b3d17a70c447bf71509f56b0539eb58dc50c
LEETaehyeon <merongvvvv@naver.com>
2020-08-10 15:05:10 +0900

    프로그래머스(Lv2)_방금 그곡_Review

---
+++ Programmers/src/Level2_Review/TEST09_2018_KAKAO_BLIND_방금_그곡_FailFailFail_Review.java
@@ -0,0 +1,22 @@
+package Level2_Review;
+
+//프로그래머스(Lv2)_방금 그곡
+public class TEST09_2018_KAKAO_BLIND_방금_그곡_FailFailFail_Review {
+	public static void main(String[] args) {
+		String m = "CC#BCC#BCC#BCC#B";
+		//String[] musicinfos = { "12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF" };
+		String[] musicinfos = { "03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B" };
+		String result = solution(m, musicinfos);
+
+		System.out.println(result);
+
+	}
+
+	public static String solution(String m, String[] musicinfos) {
+		String answer = "";
+		
+
+		return answer;
+	}
+
+}
