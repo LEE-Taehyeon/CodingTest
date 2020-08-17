bf8f6872bd5b8b0163c56dad3362a730dbe715b8
LEETaehyeon <merongvvvv@naver.com>
2020-08-17 15:17:53 +0900

    //프로그래머스(자물쇠와 열쇠)_Review

---
+++ Programmers/src/Level3_Review/TEST02_2020_KAKAO_BLIND_자물쇠와열쇠_Review_FailFailFail.java
@@ -0,0 +1,17 @@
+package Level3_Review;
+
+//프로그래머스(자물쇠와 열쇠)
+public class TEST02_2020_KAKAO_BLIND_자물쇠와열쇠_Review_FailFailFail {
+	public static void main(String[] args) {
+		int[][] key = { {0, 0, 0}, {1, 0, 0}, {0, 1, 1} };
+		int[][] lock = { {1, 1, 1}, {1, 1, 0}, {1, 0, 1} };
+		boolean result = solution(key, lock);
+		System.out.println(result);
+
+	}
+	
+	public static boolean solution(int[][] key, int[][] lock) {
+
+        return false;
+    }
+}
