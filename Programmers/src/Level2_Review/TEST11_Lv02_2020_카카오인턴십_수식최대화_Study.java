c975fd77558e2a78d61439036d8d01d7608f11ed
LEETaehyeon <merongvvvv@naver.com>
2020-08-17 16:43:32 +0900

    프로그래머스(수식최대화) - Study

---
+++ Programmers/src/Level2_Review/TEST11_Lv02_2020_카카오인턴십_수식최대화_Study.java
@@ -0,0 +1,20 @@
+package Level2_Review;
+
+//프로그래머스(수식최대화)
+public class TEST11_Lv02_2020_카카오인턴십_수식최대화_Study {
+	public static void main(String[] args) {
+		String expression = "5*100+35-8*20-2";
+		String[] nums = expression.split("[^0-9]");//0~9사이의 문자열은 제외하고 split
+		String[] signs = expression.split("[0-9]+");//0~9사이 중 하나 이상 포함한 문장열로 split
+		
+		for(String str:nums) {
+			System.out.print(str+" ");
+		}
+		
+		System.out.println();
+		
+		for(String str:signs) {
+			System.out.print(str+" ");
+		}
+	}
+}
