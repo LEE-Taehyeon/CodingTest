b481e7faa42afe8ade72a9f647396a8fd592fdcd
LEETaehyeon <merongvvvv@naver.com>
2020-08-17 16:23:57 +0900

    2020 카카오 인턴십_수식최대화

--- Programmers/src/Level2/TEST11_Lv02_2020_카카오인턴십_수식최대화_S01_V02.java
+++ Programmers/src/Level2/TEST11_Lv02_2020_카카오인턴십_수식최대화_S01_V02.java
@@ -21,7 +21,7 @@ public class TEST11_Lv02_2020_카카오인턴십_수식최대화_S01_V02 {
 		List<Character> signList = new ArrayList<Character>();
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < expression.length(); i++) {
+		for (int i = 0; i < expression.length(); i++) {//숫자와 부호를 따로 저장하기 위한 작업
 			char ch = expression.charAt(i);
 			if (!Character.isDigit(ch)) {
 				numList.add(Long.valueOf(sb.toString()));
@@ -33,24 +33,15 @@ public class TEST11_Lv02_2020_카카오인턴십_수식최대화_S01_V02 {
 		}
 		numList.add(Long.valueOf(sb.toString()));
 
-		System.out.println(numList);
-		System.out.println(signList);
-
 		char[] signArr = { '*', '+', '-' };
 		sel = new int[3];
-		permutationSort(signArr, 0, numList, signList);
+		permutationSort(signArr, 0, numList, signList);//부호의 우선순위 설정 - 조합 알고리즘
 
 		return max;
 	}
 
 	private static void permutationSort(char[] arr, int depth, List<Long> numList, List<Character> signList) {
 		if (depth == sel.length) {
-			// cnt++;
-			//System.out.println(Arrays.toString(sel));
-			for(int i=0;i<sel.length;i++) {
-				System.out.print((char)sel[i]+" ");
-			}
-			System.out.println();
 			calc(numList, signList);
 			return;
 		}
@@ -69,48 +60,42 @@ public class TEST11_Lv02_2020_카카오인턴십_수식최대화_S01_V02 {
 	private static void calc(List<Long> numList, List<Character> signList) {
 		List<Long> numListCopy = new ArrayList<Long>();
 		List<Character> signListCopy = new ArrayList<Character>();
+		
 		for(int i=0;i<numList.size();i++) {
 			numListCopy.add(numList.get(i));
 		}
 		for(int i=0;i<signList.size();i++) {
 			signListCopy.add(signList.get(i));
-		}
+		}//해당 list의 숫자와 부호를 지워가며 사칙연산의 결과를 저장하고자 해당 list들 복사
 		
-		System.out.println("--------------");
 		
 		for (int i = 0; i < sel.length; i++) {
 			for (int j = 0; j < signListCopy.size(); j++) {
-				if (sel[i] == signListCopy.get(j)) {
+				if (sel[i] == signListCopy.get(j)) {//우선순위에 맞는 부호일 때
 
 					switch (signListCopy.get(j)) {
 					case '*': {
-						numListCopy.set(j, numListCopy.get(j)*numListCopy.get(j+1));
-						//System.out.println(numListCopy.remove(j+1));
+						numListCopy.set(j, numListCopy.get(j)*numListCopy.get(j+1));//해당 연산을 한 후 j번째 값을 대체
 						break;
 					}
 					case '-': {
 						numListCopy.set(j, numListCopy.get(j)-numListCopy.get(j+1));
-						//System.out.println(numListCopy.remove(j+1));
 						break;
 					}
 					case '+': {
 						numListCopy.set(j, numListCopy.get(j)+numListCopy.get(j+1));
-						//System.out.println(numListCopy.remove(j+1));
 						break;
 					}
 
 					}
-					numListCopy.remove(j+1);
-					signListCopy.remove(j);
-					j=-1;//수정
+					numListCopy.remove(j+1);//j+1번째 원소는 연산에 참여했으므로 원소 삭제
+					signListCopy.remove(j);//해당 부호도 연산에 사용했으므로 원소 삭제
+					j=-1;//앞에서부터 다시 연산을 하기 위한 j값 설정
 				}
 			}
-			System.out.println(signListCopy);
-			System.out.println(numListCopy);
 		}
 		
-		System.out.println("Math.abs(numListCopy.get(0)):"+Math.abs(numListCopy.get(0)));
-		if(Math.abs(numListCopy.get(0))>max) {
+		if(Math.abs(numListCopy.get(0))>max) {//가장 앞에있는 값이 연산 후 결과값. 그 값이 max보다 클때 max값을 해당 값으로 변경
 			max = Math.abs(numListCopy.get(0));
 		}
 	}
