59110109c4130d4a3fb9d54ad65a010da60d2bf4
LEETaehyeon <merongvvvv@naver.com>
2020-08-18 21:52:22 +0900

    프로그래머스(징검다리 건너기)

---
+++ Programmers/src/Level3_Review/TEST03_2019_카카오개발자_겨울인턴십_징검다리건너기_Review_FailFailFail.java
@@ -0,0 +1,17 @@
+package Level3_Review;
+
+//프로그래머스(징검다리 건너기)
+public class TEST03_2019_카카오개발자_겨울인턴십_징검다리건너기_Review_FailFailFail {
+	public static void main(String[] args) {
+		//int[] stones = { 2, 4, 5, 3, 2, 1, 4, 2, 5, 1 };
+		int[] stones = { 1,1,1,1 };
+		int k = 4;
+		int result = solution(stones, k);
+		System.out.println(result);
+
+	}
+	public static int solution(int[] stones, int k) {        
+		int answer = 0;
+        return answer;
+    }
+}
